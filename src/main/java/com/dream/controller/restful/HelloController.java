a02d803eb3372752316e35e2decba23a96492143
liuxinjie123 <jackcodejs@gmail.com>
2017-12-25 23:39:52 +0800

    redis

--- src/main/java/com/dream/controller/restful/HelloController.java
+++ src/main/java/com/dream/controller/restful/HelloController.java
@@ -1,6 +1,10 @@
 package com.dream.controller.restful;
 
+import com.dream.config.RedisConfig;
 import com.dream.representation.common.result.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("helloController")
 @RequestMapping("/hello")
 public class HelloController {
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
 
     @RequestMapping(method = RequestMethod.GET)
     public Object indexMethod() {
+        stringRedisTemplate.opsForValue().set("hello", "hello jack");
         return Result.success().setData("ok");
     }
 }
